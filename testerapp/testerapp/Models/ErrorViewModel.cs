f7e805837e8f956c4368138c1f41f89c8bc10fc1
Matt <mhames80@gmail.com>
2019-09-11 16:58:47 -0400

    woooo
    ya working

---
+++ testerapp/testerapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testerapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
